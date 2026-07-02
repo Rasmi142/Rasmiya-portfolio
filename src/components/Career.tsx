07f4b63c51589f8d6bdd3f12634558a9380856e8
Rasmi142 <rasmioff@gmail.com>
2026-07-02 14:13:29 +0530

    content: update career and experience section with real data

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -15,43 +15,37 @@ const Career = () => {
           <div className="career-info-box">
             <div className="career-info-in">
               <div className="career-role">
-                <h4>Position In Company</h4>
-                <h5>Company Name</h5>
+                <h4>Frontend Developer Intern</h4>
+                <h5>Primesite Innovation</h5>
               </div>
-              <h3>20XX</h3>
+              <h3>2024</h3>
             </div>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-              labore sit non ipsum temporibus quidem, deserunt eaque officiis
-              mollitia ratione suscipit repellat.
+              Completed a three-month internship focused on frontend web development, contributing to responsive user interfaces and learning modern development workflows.
             </p>
           </div>
           <div className="career-info-box">
             <div className="career-info-in">
               <div className="career-role">
-                <h4>Position In Company</h4>
-                <h5>Company Name</h5>
+                <h4>Full Stack MERN Developer (Freelance)</h4>
+                <h5>Nous Infotech</h5>
               </div>
-              <h3>20XX</h3>
+              <h3>2024 - 2025</h3>
             </div>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-              labore sit non ipsum temporibus quidem, deserunt eaque officiis
-              mollitia ratione suscipit repellat.
+              Worked as a freelance Full Stack MERN Developer in a collaborative development team, delivering modern web applications and digital solutions for multiple clients across different industries.
             </p>
           </div>
           <div className="career-info-box">
             <div className="career-info-in">
               <div className="career-role">
-                <h4>Position In Company</h4>
-                <h5>Company Name</h5>
+                <h4>Full Stack MERN Developer</h4>
+                <h5>A4 Services</h5>
               </div>
               <h3>NOW</h3>
             </div>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-              labore sit non ipsum temporibus quidem, deserunt eaque officiis
-              mollitia ratione suscipit repellat.
+              Working as a full-time Full Stack MERN Developer, building enterprise-grade web applications for the audit, accounting, and financial services industry.
             </p>
           </div>
         </div>
